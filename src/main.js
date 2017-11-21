de7257c86680e9a679dd6b590be21ab0992a6985
nick.no.cave@gmail.com <nick.no.cave@gmail.com>
2017-11-21 21:07:37 +0200

    commit application

--- src/main.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var CashApp = require('./components/CashApp.jsx');
-
-ReactDOM.render(
-    <CashApp />,
-    document.getElementById('mount-point')
-);
